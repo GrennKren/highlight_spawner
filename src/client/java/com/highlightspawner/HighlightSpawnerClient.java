2cac35660f6fe442cc3e64659560158b422121c2
GrennKren <miosan389@gmail.com>
2025-04-17 10:14:52 +0800

    first commit - base template

---
+++ src/client/java/com/highlightspawner/HighlightSpawnerClient.java
@@ -0,0 +1,10 @@
+package com.highlightspawner;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HighlightSpawnerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
